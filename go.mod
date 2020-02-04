95e0f1b1a20d51196c8ca2bb9b6b925e6abd246f
Ricardo Geraldes <rgeraldes24@gmail.com>
2020-02-04 19:17:42 +0000

    Add base model; Add platform resources; Add string utils Join

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/videocoin/common
+
+go 1.13
+
+require (
+	github.com/jinzhu/gorm v1.9.12
+	google.golang.org/appengine v1.5.0 // indirect
+)
---
+++ strings/utils.go
@@ -0,0 +1,12 @@
+package helpers
+
+import "strings"
+
+// Join concatenates a variable number of strings.
+func Join(strs ...string) string {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(str)
+	}
+	return sb.String()
+}
