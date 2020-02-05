ec27925d750ff792b5244847425654111b638e69
Ricardo Geraldes <rgeraldes24@gmail.com>
2020-02-05 01:48:29 +0000

    resource/service: rewrote comments

--- api/resource/service/naming.go
+++ api/resource/service/naming.go
@@ -12,12 +12,12 @@ import (
 // NamePattern represents the service name pattern.
 const NamePattern = "[a-z]{3,20}.videocoin.network"
 
-// ErrInvalidName indicates that the project name is invalid.
+// ErrInvalidName indicates that the service name is invalid.
 var ErrInvalidName = resource.PatternError(NamePattern)
 
 var nameRegExp = regexp.MustCompile(strings.Join(cexp.Begin, NamePattern, cexp.End))
 
-// IsValidName verifies whether the the project name is valid or not.
+// IsValidName verifies whether the the service name is valid or not.
 func IsValidName(name string) bool {
 	return nameRegExp.MatchString(name)
 }
