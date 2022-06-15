6a93a1912c7acec02631cae33449c23157ba1734
Ricardo  Geraldes <rgeraldes24@gmail.com>
2022-06-15 14:31:08 +0400

    logging/logging: Setup add hooks as an argument

--- logging/logging.go
+++ logging/logging.go
@@ -11,18 +11,26 @@ import (
 
 // Setup configures a global logrus logger to output to stderr.
 // It populates the standard logrus logger as well as the global logging instance.
-func Setup(logLevel string) error {
+func Setup(logLevel string, hooks ...logrus.Hook) error {
 	level, err := logrus.ParseLevel(logLevel)
 	if err != nil {
 		return fmt.Errorf("error parsing log level: %v", err)
 	}
+	logrus.SetLevel(level)
+
 	hook, err := promrus.NewPrometheusHook() // Expose number of log messages as Prometheus metrics.
 	if err != nil {
 		return err
 	}
-	logrus.SetOutput(os.Stderr)
-	logrus.SetLevel(level)
 	logrus.AddHook(hook)
+
+	for _, hook := range hooks {
+		logrus.AddHook(hook)
+	}
+
+	logrus.SetOutput(os.Stderr)
+
 	SetGlobal(Logrus(logrus.StandardLogger()))
+
 	return nil
 }
